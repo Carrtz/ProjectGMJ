5249e3632fd7ce1e4cdb90dafa226980bf7cb1b0
Carrtz <157912554+Carrtz@users.noreply.github.com>
2024-06-11 08:52:05 -0300

    Update PowerUps.cs
    -Bug Fix

--- Assets/Codes/Player/PowerUps.cs
+++ Assets/Codes/Player/PowerUps.cs
@@ -5,39 +5,18 @@ using UnityEngine;
 
 public class PowerUps : MonoBehaviour
 {
-    private float originalMaxSpeed;
+    private float sumSpeed;
     private GameObject SpeedPU;
-    [SerializeField] private ScriptableStats SO;
-    // Start is called before the first frame update
-    void Start()
-    {
-        originalMaxSpeed = SO.MaxSpeed;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    private IEnumerator TemporarilyIncreaseSpeed(float newSpeed, float duration)
-    {
-        
-        SO.MaxSpeed = newSpeed;
-        Debug.Log("MaxSpeed temporariamente alterado para: " + newSpeed);
 
 
-        yield return new WaitForSeconds(duration);
-
-        SO.MaxSpeed = originalMaxSpeed;
-        Debug.Log("MaxSpeed restaurado para: " + originalMaxSpeed);
-    }
     private void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.CompareTag("Player"))
         {
-            Destroy(gameObject);
-            StartCoroutine(TemporarilyIncreaseSpeed(15f, 3f));
+        col.gameObject.GetComponent<PlayerController>().PowerUpsSpeed(sumSpeed);
+        Destroy(gameObject);
         }
+        
     }
 
 }
